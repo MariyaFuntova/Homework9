0750838a9229b27b0e8c812dccf5406bd285bd38
Mariya <m_relaxeena@mail.ru>
2022-12-08 16:25:33 +0400

    task64

---
+++ Homework9/task64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите натуральное число больше 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void NumberCounter (int N)
+{
+    if (N == 0) return;
+    Console.WriteLine($"{N} ");
+    NumberCounter(N - 1);
+}
+
+NumberCounter(N);
